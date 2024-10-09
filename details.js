28662511ba4854224b0ce53715de3e6cb15d661c
Manuel <gomez.maman24@triana.salesianos.edu>
2024-10-09 16:42:10 +0200

    Linkado todo con API, falta ponerlo bonito

--- details.js
+++ details.js
@@ -1,28 +1,50 @@
-$(document).ready(function () {
-    var urlParams = new URLSearchParams(window.location.search);
-    var pokemonId = urlParams.get("pid");
-
-    if (pokemonId == null) {
-        alert("No se ha recibido el ID de pokemon");
-        return;
+document.addEventListener('DOMContentLoaded', () => {
+    const pokemonName = getQueryParameter('name');
+    if (pokemonName) {
+        fetchPokemonData(pokemonName);
+    } else {
+        console.error('No Pokémon name found in the URL');
     }
-
-    $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
-        method: "GET",
-    }).done(function (pokemon) {
-        var template = `
-            <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
-            <div class="card-body">
-                <h5 class="card-title">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h5>
-                <p class="card-text">ID: #${String(pokemon.id).padStart(4, '0')}</p>
-                <p class="card-text">Height: ${pokemon.height}</p>
-                <p class="card-text">Weight: ${pokemon.weight}</p>
-                <p class="card-text text-start">${pokemon.description}</p>
-            </div>`;
-
-        $("#details").html(template);
-    }).fail(function () {
-        alert("Error al obtener los detalles del Pokémon");
-    });
 });
+
+function getQueryParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+function fetchPokemonData(name) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => displayPokemonData(data))
+        .catch(error => console.error('Error fetching Pokémon data:', error));
+}
+
+function displayPokemonData(data) {
+    const detailsContainer = document.getElementById('pokemon-details');
+    const pokemonDetails = `
+        
+          <img src="${data.sprites.front_default}" class="card-img-top" alt="${data.name}">
+            <div class="card-body">
+                <h5 class="card-title">${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h5>
+                <p class="card-text">ID: #${String(data.id).padStart(4, '0')}</p>
+                <p class="card-text">Height: ${data.height}</p>
+                <p class="card-text">Weight: ${data.weight}</p>
+                <p class="pokemon-types">
+                                ${data.types.map(function (typeInfo) {
+                        return '<span class="type-badge ' + typeInfo.type.name + '">' + capitalizeFirstLetter(typeInfo.type.name) + '</span>';
+                    }).join('')}
+                            </p>
+            </div>
+    `;
+    detailsContainer.innerHTML = pokemonDetails;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}   
--- index.js
+++ index.js
@@ -1,67 +1,50 @@
 $(document).ready(function () {
-    fetchPokemonData();
+    const dataContent = $('.pokemon-grid');
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=151'; // Puedes ajustar el límite según tus necesidades
 
-    $('#btn-get-data').click(function () {
-        fetchPokemonData();
-    });
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        success: function (response) {
+            const pokemonList = response.results;
 
-    function fetchPokemonData() {
-        $.ajax({
-            url: 'https://pokeapi.co/api/v2/pokemon?limit=151',
-            method: 'GET',
-            success: function (response) {
-                console.log('Fetched Pokémon list:', response.results);
-
-                // Ordenar los Pokémon por su ID
-                response.results.sort(function (a, b) {
-                    var idA = parseInt(a.url.split('/').slice(-2, -1)[0]);
-                    var idB = parseInt(b.url.split('/').slice(-2, -1)[0]);
-                    return idA - idB;
+            var requests = pokemonList.map(function (pokemon) {
+                return $.ajax({
+                    url: pokemon.url,
+                    method: 'GET'
                 });
-
-                displayPokemon(response.results);
-            },
-            error: function (error) {
-                console.error('Error fetching data:', error);
-            }
-        });
-    }
-
-    function displayPokemon(pokemonList) {
-        var dataContent = $('.pokemon-grid');
-        dataContent.empty();
-
-        var requests = pokemonList.map(function (pokemon) {
-            return $.ajax({
-                url: pokemon.url,
-                method: 'GET'
             });
-        });
 
-        $.when.apply($, requests).done(function () {
-            var responses = Array.prototype.slice.call(arguments);
-            responses.forEach(function (response) {
-                var pokemonData = response[0];
-                var pokemonCard = `
-                    <div class="pokemon-card" data-id="${pokemonData.id}">
-                        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="pokemon-image">
-                        <div class="pokemon-info">
-                            <div class="pokemon-name">${capitalizeFirstLetter(pokemonData.name)}</div>
-                            <div class="pokemon-number">#${pokemonData.id.toString().padStart(3, '0')}</div>
+            $.when.apply($, requests).done(function () {
+                var responses = Array.prototype.slice.call(arguments);
+                responses.forEach(function (response) {
+                    var pokemonData = response[0];
+                    var pokemonCard = `
+                        <div class="pokemon-card" data-id="${pokemonData.id}">
+                            <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="pokemon-image">
+                            <div class="pokemon-info">
+                                <div class="pokemon-name">
+                                    <a href="details.html?name=${pokemonData.name}">${capitalizeFirstLetter(pokemonData.name)}</a>
+                                </div>
+                                <div class="pokemon-number">#${pokemonData.id.toString().padStart(3, '0')}</div>
+                            </div>
+                            <div class="pokemon-types">
+                                ${pokemonData.types.map(function (typeInfo) {
+                        return '<span class="type-badge ' + typeInfo.type.name + '">' + capitalizeFirstLetter(typeInfo.type.name) + '</span>';
+                    }).join('')}
+                            </div>
                         </div>
-                        <div class="pokemon-types">
-                            ${pokemonData.types.map(function (typeInfo) {
-                                return '<span class="type-badge ' + typeInfo.type.name + '">' + capitalizeFirstLetter(typeInfo.type.name) + '</span>';
-                            }).join('')}
-                        </div>
-                    </div>
-                `;
-                dataContent.append(pokemonCard);
+                    `;
+                    dataContent.append(pokemonCard);
+                });
+            }).fail(function (error) {
+                console.error('Error fetching Pokémon data:', error);
             });
-        }).fail(function (error) {
-            console.error('Error fetching Pokémon data:', error);
-        });
-    }
+        },
+        error: function (error) {
+            console.error('Error fetching Pokémon list:', error);
+        }
+    });
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
