7700c0141ab3f843a4fd378c4259637a08a03b2f
Manuel Gomez <gomez.maman24@triana.salesianos.edu>
2024-10-09 12:13:11 +0200

    Mola

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     getPokemonListV2();
 
-
     $(document).on("click", "#btn-get-data", function () {
         getPokemonListV2();
     });
-
 
     function getPokemonListV2() {
         $(".pokemon-grid").html("<img src='https://i0.wp.com/www.lanaldi.es/eu/wp-content/uploads/sites/6/2017/04/cargando-1.gif?fit=441%2C291'/>");
@@ -17,21 +15,26 @@ $(document).ready(function () {
                 $(".pokemon-grid").html("");
                 var listadoPomemon = resp.results;
                 listadoPomemon.forEach(function (pokemon) {
-                    var pokemonId = pokemon.url.split("/")[6];
-                    var template = `
-                    <div class="row">
+                    var pokemonId1 = pokemon.url.split("/")[6];
+                    var pokemonId = pokemon.url.split("/")[6].padStart(4, '0');
+                    var pokemonName = capitalizeFirstLetter(pokemon.name);
 
-                        <div class="col-sm-4">
-                            <div class="pokemon-card green-background">
-                            <img src="${pokemon.id}" alt="Bulbasaur"
-                            class="pokemon-image">
-                            <h5>#0001 - ${pokemon.name}</h5>
-                            </div>
-                        </div>
-                    </div>`;
+                    var template = `
+                        <section class="pokemon-grid">
+                            <div class="pokemon-card">
+                                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId1}.png" alt="${pokemonName}" />
+                                <div class="card-body text-center">
+                                    <h5>${pokemonId} - ${pokemonName}</h5>
+                                </div>
+                             </div>
+                        </section>`;
                     $(".pokemon-grid").append(template);
                 });
-            }, 1000);
+            }, 100);
         });
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 });
